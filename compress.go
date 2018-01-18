a144cd36286ef7a1759ee43fc6500df0a3b77d17
Ethan Lynn <ethanlynnl@vmware.com>
2018-01-18 17:37:28 +0800

    Increase compress size

--- compress.go
+++ compress.go
@@ -15,23 +15,23 @@ func LZ4Compress(source []byte) ([]byte, error) {
 		fmt.Printf("Failed to compress source: %v\n", source)
 		return nil, err
 	}
-	binary.BigEndian.PutUint16(dest[n:n+2], uint16(lensrc))
-	return dest[:n+2], nil
+	binary.BigEndian.PutUint32(dest[n:n+4], uint32(lensrc))
+	return dest[:n+4], nil
 }
 
 func LZ4DeCompress(source []byte) ([]byte, error) {
-	if len(source) < 3 {
+	if len(source) < 5 {
 		return nil, fmt.Errorf("Invalid length")
 	}
-	lensrc := len(source) - 2
-	nn := binary.BigEndian.Uint16(source[lensrc:])
+	lensrc := len(source) - 4
+	nn := binary.BigEndian.Uint32(source[lensrc:])
 	dest := make([]byte, nn)
 	n, err := lz4.DecompressSafe(source[:lensrc], dest)
 	if err != nil {
 		fmt.Printf("Failed to decompress source: %v\n", source)
 		return nil, err
 	}
-	if uint16(n) != nn {
+	if uint32(n) != nn {
 		fmt.Printf("Size after decompress %d is not the same as %s.\n", n, nn)
 		return nil, fmt.Errorf("Error on decompress packet size")
 	}
